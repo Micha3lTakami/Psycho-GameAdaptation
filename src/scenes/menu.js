c5af65f262bdd5a0aad17c1e5e9296e2b224dee5
Michael Takami <mtakami@ucsc.edu>
2023-06-04 23:26:50 -0700

    many scenes

---
+++ src/scenes/menu.js
@@ -0,0 +1,47 @@
+class Menu extends Phaser.Scene{
+    constructor(){
+        super('menuScene')
+    }
+    // create()
+    // create start scene
+    create() {
+        
+        // 8-bit background
+        let batesBCKG = this.add.image(w/3, h/2, 'menu');
+        batesBCKG.setScale(0.65);
+
+        // tweened logo
+        const logoStyle = { fontFamily: 'Impact', fontSize: '48px', color: '#732002' };
+        const logo = this.add.text(w*.1, h*.15, 'Psycho', logoStyle);
+        this.tweens.add({
+          targets: logo,
+          scaleY: { value: 1.05, yoyo: true, duration: 1000, ease: 'Sine.easeIn' },
+          y: '+=10',
+          yoyo: true,
+          repeat: -1,
+          duration: 3000,
+          ease: 'Sine.easeInOut',
+        });
+
+        // directions
+        const directionStyle = { fontFamily: 'Courier', fontSize: '16px', color: '#f29057' };
+        const directions = this.add.text(w*.1, h*.5, 'Use [W,A,S,D] to move', directionStyle);
+        const directions2 = this.add.text(w*.1, h*.6,'[SPACE] to interact', directionStyle);
+
+    }
+
+    // update()
+    // menu update function
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyENTER)) {
+            //this.sound.play('select');
+            this.cameras.main.fadeOut(1000,10,20,30,);
+            this.time.delayedCall(1100, () =>{
+                // restyle border for next scene
+                let canvas = document.querySelector('canvas');
+                canvas.style.border = '10px #ADD8E6 inset';   
+                this.scene.start('introScene'); 
+            });
+        }
+    }
+}
